ab71da43f587e3d365ad1ca6a8eef38484b670c0
Hung Cao <caominhhung1991@gmail.com>
2018-03-08 18:02:03 +0700

    123

--- src/main/java/com/teamtreehouse/giflib/controller/GifController.java
+++ src/main/java/com/teamtreehouse/giflib/controller/GifController.java
@@ -1,15 +1,32 @@
 package com.teamtreehouse.giflib.controller;
 
+import com.teamtreehouse.giflib.data.GifRepository;
+import com.teamtreehouse.giflib.model.Gif;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDate;
 
 /**
  * Created by JavaScript on 3/7/2018.
  */
 @Controller
 public class GifController {
+    @Autowired
+    private GifRepository gifRepository;
+
     @RequestMapping("/")
     public String listGifs() {
         return "home";
+    }
+
+    @RequestMapping("/gif/{name}")
+    public String gifDetails(@PathVariable String name, ModelMap modelMap) {
+        Gif gif = gifRepository.findByName(name);
+        modelMap.put("gif", gif);
+        return "gif-details";
     }
 }
